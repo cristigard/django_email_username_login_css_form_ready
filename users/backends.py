ee2cb198bb0b3052b51203485451553a7ff35fbd
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2023-04-09 22:50:48 +0300

    Update backends.py

--- users/backends.py
+++ users/backends.py
@@ -8,13 +8,15 @@ UserModel = get_user_model()
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return None
         try:
             user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            print(user)
         except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
             UserModel().set_password(password)
-            return None
-        except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
-
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
